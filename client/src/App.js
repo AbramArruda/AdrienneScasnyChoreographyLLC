10b3ddd3445137447eb38051ad8ac2234502e20d
Abram <abramarruda@gmail.com>
2019-01-06 23:53:22 -0500

    changes

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import logo from "./logo.jpg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>2019 aint ready doooooeee</p>
+        </header>
+      </div>
+    );
+  }
+}
+//lehhgoooooooooo
+export default App;
