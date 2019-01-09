3ea219b0686faaa75b74f5c93f21ce6da08f2f8a
Abram <abramarruda@gmail.com>
2019-01-08 22:42:29 -0500

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>2019 aint ready doooooeee</p>
+          <p>Dance Choreographer</p>
         </header>
       </div>
     );
