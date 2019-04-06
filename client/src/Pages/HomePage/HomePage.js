c2b8cc3e1620061e781be281730aab8458237e1b
Abram <abramarruda@gmail.com>
2019-04-06 17:29:50 -0400

    changes

--- client/src/Pages/HomePage/HomePage.js
+++ client/src/Pages/HomePage/HomePage.js
@@ -2,9 +2,7 @@ import React, { Component } from "react";
 import Fade from "react-reveal/Fade";
 import Pulse from "react-reveal/Pulse";
 import "./HomePage.css";
-import TextLoop from "react-text-loop";
 import Footer from "../../Components/Footer";
-import Animation from "../../Components/Animation";
 
 export default class HomePage extends Component {
   render() {
@@ -15,27 +13,12 @@ export default class HomePage extends Component {
             <h1 id="homeText">
               {" "}
               <Fade cascade left duration={1500} delay={500}>
-                Hi, I'm Abram Arruda
+                Adrienne Scasny
               </Fade>
               <Fade cascade duration={1500} delay={1700}>
-                <h3 id="homeText2">
-                  <TextLoop
-                    springConfig={{ stiffness: 40, damping: 30 }}
-                    speed={8000}
-                    children={[
-                      "web developer",
-                      "problem solver",
-                      "biologist",
-                      "soccer player",
-                      "lifelong learner"
-                    ]}
-                  />
-                </h3>
+                <h3 id="homeText2" />
               </Fade>
             </h1>
-          </div>
-          <div class="col-sm-12 col-lg-5" id="homeImage">
-            <Animation />
           </div>
         </div>
         <Footer />
