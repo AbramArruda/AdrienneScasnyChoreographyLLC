5651da41776dd0b31c4cad4cefa69c60c9d593ad
Abram <abramarruda@gmail.com>
2019-04-19 22:16:02 -0400

    Updates

---
+++ precache-manifest.0758b03898cb5581f925d173f72e33c1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "fed131452d78c6201b0deae44134c811",
+    "url": "/static/media/badnboujee.fed13145.jpeg"
+  },
+  {
+    "revision": "09db688d19bf9a702bbe",
+    "url": "/static/css/main.1ed48078.chunk.css"
+  },
+  {
+    "revision": "9e6ccda77a8cd718002f",
+    "url": "/static/js/1.9e6ccda7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d94f657036e4feffc36bcb17b09b9596",
+    "url": "/static/media/LOGO_B.d94f6570.png"
+  },
+  {
+    "revision": "fa2eb309e6fe115ede41acc449046a82",
+    "url": "/static/media/lookinforbae.fa2eb309.jpeg"
+  },
+  {
+    "revision": "09db688d19bf9a702bbe",
+    "url": "/static/js/main.09db688d.chunk.js"
+  },
+  {
+    "revision": "d5e37a563de580d0104f5f0e8c6c48ab",
+    "url": "/static/media/lol.d5e37a56.JPG"
+  },
+  {
+    "revision": "50bbeeee6dd59799761a02762fcc145f",
+    "url": "/static/media/Garage.50bbeeee.JPG"
+  },
+  {
+    "revision": "82ac6f89b63e30a90c9a4dc743a32dad",
+    "url": "/static/media/City.82ac6f89.JPG"
+  },
+  {
+    "revision": "c98bb692c6ab75fea5be00672c1f87c8",
+    "url": "/static/media/boo.c98bb692.JPG"
+  },
+  {
+    "revision": "8a6d2c60b91e29122424c5a068af764b",
+    "url": "/static/media/Jump.8a6d2c60.jpeg"
+  },
+  {
+    "revision": "860e3737e636416010b4626bbdfe281b",
+    "url": "/index.html"
+  }
+];
